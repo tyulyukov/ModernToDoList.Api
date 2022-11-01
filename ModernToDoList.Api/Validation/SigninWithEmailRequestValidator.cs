50f8e29b3167dd7a0b683efe32921aa68e206646
tyulyukov <makstyulyukov@gmail.com>
2022-11-01 15:34:35 +0200

    Sign In Requests Validators

---
+++ ModernToDoList.Api/Validation/SigninWithEmailRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using ModernToDoList.Api.Domain.Contracts.Requests;
+
+namespace ModernToDoList.Api.Validation;
+
+public class SigninWithEmailRequestValidator : Validator<SigninWithEmailRequest>
+{
+    public SigninWithEmailRequestValidator()
+    {
+        RuleFor(x => x.Password).NotEmpty();
+        RuleFor(x => x.EmailAddress).NotEmpty();
+    }
+}
---
+++ ModernToDoList.Api/Validation/SigninWithUsernameRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using ModernToDoList.Api.Domain.Contracts.Requests;
+
+namespace ModernToDoList.Api.Validation;
+
+public class SigninWithUsernameRequestValidator : Validator<SigninWithUsernameRequest>
+{
+    public SigninWithUsernameRequestValidator()
+    {
+        RuleFor(x => x.Password).NotEmpty();
+        RuleFor(x => x.Username).NotEmpty();
+    }
+}
